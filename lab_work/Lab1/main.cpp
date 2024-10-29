ab8c3e7a076ee2f713b458f69379bdde73c0b794
IvannZ <zhukov.ivan05@yandex.ru>
2024-10-29 15:24:50 +0700

    fulli make Lab1 in linux

--- lab_work/Lab1/main.cpp
+++ lab_work/Lab1/main.cpp
@@ -7,6 +7,21 @@ void DrawPoint(SDL_Renderer *renderer, int x, int y, int squareSize,
                int numSquares);
 
 int main() {
+
+  int numSquares;     // Количество квадратов в сетке
+  int squareSize;     // Размер одного квадрата
+  int pointX, pointY; // Координаты точки
+
+  std::cout << "Enter number of squares in grid: ";
+  std::cin >> numSquares;
+  std::cout << "Enter the size of one square: ";
+  std::cin >> squareSize;
+  std::cout
+      << "Enter the coordinates of the point (X and Y separated by a space): ";
+  std::cin >> pointX >> pointY;
+  pointY--;
+  pointX--;
+
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     std::cerr << "Ошибка инициализации SDL: " << SDL_GetError() << std::endl;
     return 1;
@@ -14,7 +29,7 @@ int main() {
 
   SDL_Window *window =
       SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+                       SDL_WINDOWPOS_CENTERED, numSquares*squareSize, numSquares*squareSize, SDL_WINDOW_SHOWN);
   if (!window) {
     std::cerr << "Ошибка создания окна: " << SDL_GetError() << std::endl;
     SDL_Quit();
@@ -29,11 +44,11 @@ int main() {
     SDL_Quit();
     return 1;
   }
+
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
-
-  DrawGrid(renderer, 10, 30);
-  DrawPoint(renderer, 1, 2, 30, 10);
+  DrawGrid(renderer, numSquares, squareSize);
+  DrawPoint(renderer, pointX, pointY, squareSize, numSquares);
   SDL_RenderPresent(renderer);
   int a;
   std::cin >> a;
